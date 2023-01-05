82ca2d9cea297889ba80f5eca7694cc7c46e5aa0
F3ll3n <fellenyoup@gmail.com>
2023-01-05 21:03:05 +0300

    pagination added

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import ReactPaginate from "react-paginate";
 import styles from "./Pagination.module.scss";
 
-const Pagination = () => {
+const Pagination = ({ onChangePage }) => {
   return (
     <ReactPaginate
       className={styles.root}
       breakLabel="..."
       nextLabel=">"
-      onPageChange={event => console.log(event)}
-      pageRangeDisplayed={8}
+      onPageChange={event => onChangePage(event.selected + 1)}
+      pageRangeDisplayed={4}
       pageCount={3}
       previousLabel="<"
       renderOnZeroPageCount={null}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,8 @@ import Sort from "../components/Sort/Sort";
 import Pagination from "../components/Pagination/Pagination";
 
 const Home = ({ searchValue }) => {
-  //TODO: Filter by category
   const [items, setItems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
   const [sortType, setSortType] = useState({
@@ -24,7 +24,7 @@ const Home = ({ searchValue }) => {
     const search = searchValue ? `search=${searchValue}` : "";
     setIsLoading(true);
     fetch(
-      `https://63ab80f2fdc006ba605f873f.mockapi.io/items?${category}&sortBy=${sort}&order=${order}${search}`,
+      `https://63ab80f2fdc006ba605f873f.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sort}&order=${order}&${search}`,
     )
       .then(res => {
         return res.json();
@@ -33,23 +33,23 @@ const Home = ({ searchValue }) => {
         setItems(json);
         setIsLoading(false);
       });
-  }, [sortType, categoryId, searchValue]);
+  }, [sortType, categoryId, searchValue, currentPage]);
   const pizzas = items.map((pizzaItem, index) => (
     <PizzaBlock {...pizzaItem} key={index} />
   ));
   const skeletons = [...new Array(8)].map((_, index) => (
     <Skeleton key={index} />
   ));
-
+  //TODO: Hide pagination if backend have < 2 pages
   return (
     <div className="container">
       <div className="content__top">
         <Categories onSelectCategory={id => setCategoryId(id)} />
         <Sort handleSort={value => setSortType(value)} />
       </div>
-      <h2 className="content__title">Все пиццы"</h2>
+      <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">{isLoading ? skeletons : pizzas}</div>
-      <Pagination />
+      <Pagination onChangePage={(number => setCurrentPage(number))}/>
     </div>
   );
 };
